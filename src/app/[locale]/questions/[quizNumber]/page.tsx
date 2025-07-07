971d29c20490678317bb4b6fa7c6c4f99bfce984
hossein seyedi <hosseincybercoin@gmail.com>
2025-07-07 14:18:02 -0700

    add transition to quiz number page

--- src/app/[locale]/questions/[quizNumber]/page.tsx
+++ src/app/[locale]/questions/[quizNumber]/page.tsx
@@ -11,6 +11,7 @@ import { useQuestionsStore } from "@/stores/questions";
 import { useApi } from "@/context/api";
 import { useRouter } from "@/i18n/navigation";
 import { IoArrowBackSharp } from "react-icons/io5";
+import { motion, AnimatePresence } from "framer-motion";
 
 type QuizOption = {
   id: number;
@@ -118,56 +119,63 @@ function Page() {
   }
 
   return (
-    <div>
-      <div className="w-full flex items-center justify-start my-4  px-4 ">
-        <button
-          className="p-2 text-white bg-gray-800 rounded-full border border-[#C643A8E5] "
-          onClick={() => router.push("/questions")}
-        >
-          <IoArrowBackSharp></IoArrowBackSharp>
-        </button>
-      </div>
-      {currentQuestion.imageUrl && (
-        <div
-          style={{ position: "relative" }}
-          className="w-full h-[300px] absolute top-0 left-0 right-0 "
-        >
-          <Image
-            src={currentQuestion.imageUrl}
-            alt="quiz"
-            layout="fill"
-            objectFit="contain"
-            priority
-          />
+    <AnimatePresence mode="wait">
+      <motion.div
+        key={currentIndex}
+        initial={{ opacity: 0, x: 50 }}
+        animate={{ opacity: 1, x: 0 }}
+        exit={{ opacity: 0, x: -50 }}
+        transition={{ duration: 0.3 }}
+      >
+        <div className="w-full flex items-center justify-start my-4  px-4 ">
+          <button
+            className="p-2 text-white bg-gray-800 rounded-full border border-[#C643A8E5] "
+            onClick={() => router.push("/questions")}
+          >
+            <IoArrowBackSharp></IoArrowBackSharp>
+          </button>
         </div>
-      )}
-      <p className="text-center text-[16px] font-[600] py-3">
-        {currentQuestion.text}
-      </p>
-
-      <div className="flex flex-col items-center justify-center mt-4 gap-2">
-        {currentQuestion.options.map((option, index) => {
-          let state: "default" | "correct" | "incorrect" = "default";
-          if (selectedAnswer === option.id) {
-            state =
-              isCorrect === true
-                ? "correct"
-                : isCorrect === false
-                ? "incorrect"
-                : "default";
-          }
-          return (
-            <div key={option.id} className="w-full">
-              <AnswerItem
-                text={option.text}
-                onClick={() => handleAnswer(option.id, option.isCorrect)}
-                state={state}
-              />
-            </div>
-          );
-        })}
-      </div>
-    </div>
+        {currentQuestion.imageUrl && (
+          <div
+            style={{ position: "relative" }}
+            className="w-full h-[300px] absolute top-0 left-0 right-0 "
+          >
+            <Image
+              src={currentQuestion.imageUrl}
+              alt="quiz"
+              layout="fill"
+              objectFit="contain"
+              priority
+            />
+          </div>
+        )}
+        <p className="text-center text-[16px] font-[600] py-3">
+          {currentQuestion.text}
+        </p>
+        <div className="flex flex-col items-center justify-center mt-4 gap-2">
+          {currentQuestion.options.map((option, index) => {
+            let state: "default" | "correct" | "incorrect" = "default";
+            if (selectedAnswer === option.id) {
+              state =
+                isCorrect === true
+                  ? "correct"
+                  : isCorrect === false
+                  ? "incorrect"
+                  : "default";
+            }
+            return (
+              <div key={option.id} className="w-full">
+                <AnswerItem
+                  text={option.text}
+                  onClick={() => handleAnswer(option.id, option.isCorrect)}
+                  state={state}
+                />
+              </div>
+            );
+          })}
+        </div>
+      </motion.div>
+    </AnimatePresence>
   );
 }
 
