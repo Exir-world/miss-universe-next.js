926c45c693327458b375ee90446f3d16e22e88fe
hossein seyedi <hosseincybercoin@gmail.com>
2025-07-08 14:00:25 -0700

    fix image address in home page

--- src/components/clientWrappper.tsx
+++ src/components/clientWrappper.tsx
@@ -2,12 +2,15 @@
 import { useEffect } from "react";
 import { useLoginStoreState } from "@/stores/context";
 
-export default function ClientWrapper({ children }: { children: React.ReactNode }) {
+export default function ClientWrapper({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   const { getMe } = useLoginStoreState();
 
   useEffect(() => {
     const callMe = async () => {
-      console.log("from wrapper");
       await getMe();
     };
     callMe();
