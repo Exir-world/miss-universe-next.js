fceeaaf8cf367728257482f8a30a41657c6c922f
Mahdiyar Bafandeh <mahdybozorgmehr@gmail.com>
2025-07-09 15:23:02 +0330

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,8 @@ const nextConfig: NextConfig = {
   env: {
     NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
     NEXT_PUBLIC_GAME_NAME: process.env.NEXT_PUBLIC_GAME_NAME,
+    NEXT_GAME_NAME: process.env.NEXT_GAME_NAME,
+    NEXT_REFERRAL_URL: process.env.NEXT_REFERRAL_URL,
   },
   reactStrictMode: false,
   typescript: {
