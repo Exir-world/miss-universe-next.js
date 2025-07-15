04308651adbbe5d238e629671b313f53f888180e
hossein seyedi <hosseincybercoin@gmail.com>
2025-07-15 14:39:14 -0700

    fix referral url

--- src/stores/login.ts
+++ src/stores/login.ts
@@ -196,7 +196,7 @@ export function createLoginStore(api?: AxiosInstance): StoreApi<LoginStore> {
             // Get referral code from URL if available
             const urlParams = new URLSearchParams(window.location.search);
 
-            const referralCode = urlParams.get("start");
+            const referralCode = urlParams.get("r") || urlParams.get("start");
 
             await axiosInstance.post("/mainuser/join", {
               ...(referralCode ? { referralCode } : {}),
